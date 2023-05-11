48613cde50271684e0d6070d66f699a44c977782
nguyendongminhtri29092014@gmail.com <nguyendongminhtri29092014@gmail.com>
2023-05-11 17:41:19 +0700

    update bien moi truong

--- src/main/java/rikkei/academy/config/ConnectMySQL.java
+++ src/main/java/rikkei/academy/config/ConnectMySQL.java
@@ -5,11 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 public class ConnectMySQL {
     private static Connection connection;
-    private static final String URL = "jdbc:mysql://localhost:3306/form_login_jv10";
-    private static final String USER = "root";
-    private static final String PASS = "Minhtri29092014";
+    private static final String URL = System.getenv("URL");
+    private static final String USER = System.getenv("USER");
+    private static final String PASS = System.getenv("PASS");
     public static Connection getConnection(){
         try {
+            System.out.println("URL --->"+URL);
+            System.out.println("USER --->"+USER);
+            System.out.println("PASS -->"+PASS);
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(URL,USER,PASS);
             System.out.println("KET NOI THANH CONG");
